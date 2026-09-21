60865dfee90e07391f068950965582878fa050ad
 Maria Pashchevska <marijka@Marias-MacBook-Pro.local>
2026-09-21 17:33:59 +0300

    Format with Prettier

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,16 +1,16 @@
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
 const textarea = form.elements.message;
-const localStorageKey = "feedback-form-state";
-let formData = { email: "", message: "" };
+const localStorageKey = 'feedback-form-state';
+let formData = { email: '', message: '' };
 
 try {
   const savedData = JSON.parse(localStorage.getItem(localStorageKey));
   if (savedData) {
-    formData.email = savedData.email || "";
-    formData.message = savedData.message || "";
+    formData.email = savedData.email || '';
+    formData.message = savedData.message || '';
   }
 } catch (error) {
-  console.error("Error parsing localStorage data:", error);
+  console.error('Error parsing localStorage data:', error);
 }
 
 textarea.value = formData.message;
@@ -18,21 +18,21 @@ if (formData.email) {
   form.elements.email.value = formData.email;
 }
 
-form.addEventListener("input", (evt) => {
+form.addEventListener('input', evt => {
   formData[evt.target.name] = evt.target.value.trim();
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
-form.addEventListener("submit", (evt) => {
+form.addEventListener('submit', evt => {
   evt.preventDefault();
-  
+
   if (!formData.email || !formData.message) {
-    alert("Fill please all fields");
+    alert('Fill please all fields');
     return;
   }
 
   console.log(formData);
   localStorage.removeItem(localStorageKey);
   form.reset();
-  formData = { email: "", message: "" };
+  formData = { email: '', message: '' };
 });
